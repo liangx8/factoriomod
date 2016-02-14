7dcc9c392d86694002063e22f3c7740ce6ad30a6
wang.gang(archlinux) <wang.gang@home.com>
2016-02-14 21:46:51 +0800

    change recipe of alien artifact

--- myitem_0.0.1/prototypes/recipes.lua
+++ myitem_0.0.1/prototypes/recipes.lua
@@ -5,13 +5,31 @@ data:extend({
 	enabled = true,
 	ingredients =
 	{
-		{"iron-plate",1},
-		{"copper-plate",1}
+		{"iron-ore",1},
+		{"copper-ore",1}
 	},
 	energy_required = 15,
 	category = "advanced-crafting",
 
 	result="my-coin"
+  },
+  {
+	type="recipe",
+	name="coin-mime",
+	icon = "__base__/graphics/icons/coin.png",
+	enabled = true,
+	ingredients =
+	{
+		{"my-coin",1}
+	},
+	energy_required = 15,
+	category = "advanced-crafting",
+	subgroup = "other",
+	results =
+	{
+		{type="item",name="iron-ore",amount=1},
+		{type="item",name="copper-ore",amount=1}
+	}
   },
   {
 	type="recipe",
