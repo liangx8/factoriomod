bd5f66485910918e2c1dfbdbad5520112ddef77d
wang gang(office linux) <wang.gang@192.168.30.1>
2016-10-15 10:15:12 +0800

    for 0.14

--- myitem_0.0.1/data.lua
+++ myitem_0.0.1/data.lua
@@ -4,32 +4,6 @@ require("prototypes.platform-entities")
 require("prototypes.items")
 require("prototypes.recipes")
 
-data.raw["player"]["player"].inventory_size=120
-data.raw["recipe"]["straight-rail"].result_count = 200
-data.raw["recipe"]["curved-rail"].result_count = 20
-data.raw["item"]["straight-rail"].stack_size = 1000
-data.raw["item"]["curved-rail"].stack_size = 200
-
-data.raw["recipe"]["smart-inserter"].ingredients=
-{
-{"iron-plate",24},
-{"copper-plate",21}
-}
-data.raw["recipe"]["smart-inserter"].result_count = 2
-
-data.raw["recipe"]["science-pack-1"].ingredients=
-{
-{"iron-plate",3}
-}
-data.raw["recipe"]["science-pack-2"].ingredients=
-{
-{"iron-plate",14},
-{"copper-plate",3}
-}
-data.raw["recipe"]["science-pack-2"].result_count = 2
-data.raw["recipe"]["advanced-circuit"].ingredients=
-{
-{"iron-plate",2},
-{"copper-plate",5},
-{"plastic-bar",2}
-}
+data.raw.item["coal"].stack_size       = 500
+data.raw.item["copper-ore"].stack_size = 500
+data.raw.item["iron-ore"].stack_size   = 500
--- myitem_0.0.1/prototypes/recipes.lua
+++ myitem_0.0.1/prototypes/recipes.lua
@@ -1,50 +1,5 @@
 data:extend({
-	{
-	type="recipe",
-	name="my-coin",
-	enabled = true,
-	ingredients =
-	{
-		{"iron-ore",1},
-		{"copper-ore",1}
-	},
-	-- use default
-	--	energy_required = 15,
-	category = "advanced-crafting",
 
-	result="my-coin",
-	result_count=2
-  },
-  {
-	type="recipe",
-	name="coin-mime",
-	icon = "__base__/graphics/icons/coin.png",
-	enabled = true,
-	ingredients =
-	{
-		{"my-coin",10}
-	},
-	--	energy_required = 15,
-	category = "advanced-crafting",
-	subgroup = "other",
-	results =
-	{
-		{type="item",name="iron-ore",amount=5},
-		{type="item",name="copper-ore",amount=5}
-	}
-  },
-  {
-	type="recipe",
-	name="man-make-artifact",
-	enabled = true,
-	ingredients =
-	{
-		{"my-coin",20}
-	},
-	energy_required = 60,
---	category = "advanced-crafting",
-	result="alien-artifact"
-  },
   {
 	type="recipe",
 	name="cargo-platform-v",
@@ -88,21 +43,6 @@ data:extend({
 	},
 	energy_required = 1,
 	result = "cargo-platform-h"
-  },
-  {
-	type="recipe",
-	name="adv-smart-inserter",
-	icon = "__base__/graphics/icons/smart-inserter.png",
-	enabled = enable,
-	subgroup = "other",
-	ingredients = {
-		{type = "item", name = "iron-plate", amount = 24},
-		{type = "item", name = "copper-plate", amount =21}
-
-	},
-		results =
-		{
-		{"smart-inserter",2}
-		}
   }
+
 })
