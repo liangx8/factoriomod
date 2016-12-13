b8f636caaa3c5bc8cd72c7bd6b45355093cd7e38
Rose Wang <rose.wang@athome.com>
2016-12-13 08:41:45 +0800

    skip begin

---
+++ fastskip_1.0.0/prototypes/recipes.lua
@@ -0,0 +1,39 @@
+data:extend({
+  {
+    type="recipe",
+	name="temp-science-pack-1",
+	energy_required = 1,
+	ingredients = {{"iron-plate",1}},
+	enable = true,
+	result = "science-pack-1"
+  },
+  {
+    type="recipe",
+	name="temp-science-pack-2",
+	energy_required = 1,
+	ingredients = {{"copper-plate",1}},
+	enable = true,
+	result = "science-pack-2"
+  },
+  {
+    type="recipe",
+	name="temp-science-pack-3",
+	energy_required = 1,
+	ingredients = {
+	  {"iron-plate",1},
+	  {"copper-plate",1}
+	},
+	enable = true,
+	result = "science-pack-3"
+  },
+  {
+    type="recipe",
+	name="temp-alien-science-pack",
+	energy_required = 1,
+	ingredients = {
+	  {"stone-brick",1}
+	},
+	enable = true,
+	result = "alien-science-pack"
+  }
+})
