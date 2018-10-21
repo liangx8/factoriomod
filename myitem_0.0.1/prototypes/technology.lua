3a5f34f2cc73c58830b86f61065604037dcaf3f9
wang gang <wang.home.gang@linuxbox.com>
2018-10-21 15:43:49 +0800

    change recipe of science-pack-3, military-science-pack,high-tech-science-pack,advanced-circuit

--- myitem_0.0.1/prototypes/technology.lua
+++ myitem_0.0.1/prototypes/technology.lua
@@ -1,60 +1,6 @@
 data:extend({
   {
     type = "technology",
-    name = "long-chest-tech",
-    icon = "__myitem__/graphics/icon/long_chest_tech.png",
-	icon_size = 32,
-    effects =
-    {
-      {
-        type = "unlock-recipe",
-        recipe = "cargo-platform-v"
-      },
-      {
-        type = "unlock-recipe",
-        recipe = "cargo-platform-h"
-      },
-      {
-        type = "unlock-recipe",
-        recipe = "cargo-platform-rotate-hv"
-      },
-      {
-        type = "unlock-recipe",
-        recipe = "cargo-platform-rotate-vh"
-      },
-
-
-      {
-        type = "unlock-recipe",
-        recipe = "cargo-platform-v4"
-      },
-      {
-        type = "unlock-recipe",
-        recipe = "cargo-platform-h4"
-      },
-      {
-        type = "unlock-recipe",
-        recipe = "cargo-platform-rotate-hv4"
-      },
-      {
-        type = "unlock-recipe",
-        recipe = "cargo-platform-rotate-vh4"
-      }
-
-
-
-    },
-    prerequisites = {"automation", "steel-processing"},
-    unit =
-    {
-      count = 20,
-      ingredients = {{"science-pack-1", 1}},
-      time = 5
-    },
-    order = "c-a"
-  },
-  {
-    type = "technology",
     name = "loader",
     icon = "__base__/graphics/icons/loader.png",
     icon_size=32,
@@ -130,3 +76,4 @@ data:extend({
   }
 
 })
+
