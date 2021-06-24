0987a26ec6cd53bd1d7ab11da3060953aa52ffc2
Mr. Leung <ling.home.worker@home.com>
2021-06-24 16:24:52 +0800

    good

---
+++ 1.0.0/prototypes/recipes.lua
@@ -0,0 +1,23 @@
+-- data:extend({})
+--[[
+data.raw.recipe["science-pack-3"].ingredients={
+	{"science-pack-1",4},
+	{"science-pack-2",10},
+	{"plastic-bar", 4},
+	{"iron-plate",5}
+}
+data.raw.recipe["science-pack-3"].results={{"science-pack-3",2}}
+data.raw.recipe["science-pack-3"].energy_required = 40
+data.raw.recipe["military-science-pack"].ingredients={
+	{"science-pack-1",6},
+	{"science-pack-2",6},
+	{"grenade", 1},
+	{"iron-plate",4}
+}
+data.raw.recipe["military-science-pack"].energy_required = 20
+--]]
+data.raw.recipe["advanced-circuit"].normal.ingredients={
+  {"electronic-circuit",2},
+  {"plastic-bar",2},
+  {"copper-plate",2}
+}
---
+++ 1.1/prototypes/technology.lua
@@ -0,0 +1,73 @@
+data:extend({
+  {
+    type = "technology",
+    name = "loader",
+    icon = "__base__/graphics/icons/loader.png",
+    icon_size=32,
+    effects = 
+    {
+       {
+         type = "unlock-recipe",
+          recipe = "loader"
+       }
+    },
+    unit = 
+    {
+      count = 500,
+       ingredients = 
+       {
+        {"automation-science-pack",1}
+       },
+        time = 30
+    }
+
+  },  {
+    type = "technology",
+    name = "fast-loader",
+    icon = "__base__/graphics/icons/fast-loader.png",
+    icon_size=32,
+    effects = 
+    {
+       {
+         type = "unlock-recipe",
+         recipe = "fast-loader"
+       }
+    },
+    prerequisites = {"loader","automation-3"},
+    unit = 
+    {
+      count = 600,
+       ingredients = 
+       {
+        {"automation-science-pack",1}
+       },
+        time = 30
+    }
+
+  }, {
+    type = "technology",
+    name = "express-loader",
+    icon = "__base__/graphics/icons/express-loader.png",
+    icon_size=32,
+    effects = 
+    {
+       {
+         type = "unlock-recipe",
+         recipe = "express-loader"
+       }
+    },
+    prerequisites = {"fast-loader"},
+    unit = 
+    {
+      count = 700,
+       ingredients = 
+       {
+        {"automation-science-pack",1}
+       },
+        time = 30
+    }
+
+  }
+
+})
+
